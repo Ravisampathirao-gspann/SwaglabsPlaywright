ad3ecc68eaa85302801537bf1048bd9697763d8b
anilbade-gspann <anil.bade@gspann.com>
2025-08-21 17:08:31 +0530

    cart functionality code commited

--- tests/pageFactory.js
+++ tests/pageFactory.js
@@ -1,4 +1,6 @@
 import {test as base,expect} from '@playwright/test'
+import { cartPage } from '../pages/cartPage'
+import { loginPage } from '../pages/loginPage'
 
 export const test=base.extend({
     loginpage:async ({page},use)=>{
@@ -20,6 +22,17 @@ export const test=base.extend({
         await loginpage.applaunching()
         await loginpage.loginToAccount("visual_user","secret_sauce")
         await loginpage.clickLogin()
+        await use();
+    },
+    cartPageSetUp:async({cartpage},use)=>{
+        await cartpage.selectItem()
+        await cartpage.addingItemToCart()
+        await use();
+    },
+    cartPageSetUpSecondItem:async({cartpage},use)=>{
+        await cartpage.clickBackToProducts()
+        await cartpage.selectTshirt()
+        await cartpage.addingItemToCart()
         await use();
     }
 })
