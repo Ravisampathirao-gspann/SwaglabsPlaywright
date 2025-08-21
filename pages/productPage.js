eb6ad3cfcc222db102ced37fbe1c6a41389b9ed4
Shagun Singhal <shagun.singhal@gspann.com>
2025-08-21 15:56:28 +0530

    Added 5 test cases for inventory page

--- pages/productPage.js
+++ pages/productPage.js
@@ -7,41 +7,71 @@ export class ProductPage {
     this.backpackPrice = page.getByText('$29.99');
     this.addToCartButton = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]');
     this.cartIcon = page.locator('[data-test="shopping-cart-link"]');
+
+    this.swagLabsText = page.getByText('Swag Labs');
+    this.openMenuButton = page.getByRole('button', { name: 'Open Menu' });
+    this.inventorySidebarLink = page.locator('[data-test="inventory-sidebar-link"]');
+    this.aboutSidebarLink = page.locator('[data-test="about-sidebar-link"]');
+    this.logoutSidebarLink = page.locator('[data-test="logout-sidebar-link"]');
+    this.resetSidebarLink = page.locator('[data-test="reset-sidebar-link"]');
   }
 
   async verifyBackpackImage() {
-    
+
     return await this.backpackImage.isVisible()
   }
   async verifyBackpackTitle() {
-    
+
     return await this.backpackTitle.isVisible()
   }
   async verifyBackpackDescription() {
-    
+
     return await this.backpackDescription.isVisible()
   }
   async verifyBackpackPrice() {
-    
+
     return await this.backpackPrice.isVisible()
-    
+
   }
   async verifyAddToCartButton() {
-    
+
     return await this.addToCartButton.isVisible()
 
   }
 
-  async verifyInventoryPageTitle(){
-    return await this.InventoryPageTitle.isVisible()
-  }
+
   async addToCart() {
     await this.addToCartButton.click();
     await this.cartIcon.click();
+  }
+
+  async verifySwagLabsText() {
+    return await this.swagLabsText.isVisible();
+  }
+
+  async openMenu() {
+    await this.openMenuButton.waitFor({ state: 'visible', timeout: 5000 });
+    await this.openMenuButton.click();
+  }
+
+  async verifyInventorySidebarLink() {
+    return await this.inventorySidebarLink.isVisible();
+  }
+
+  async verifyAboutSidebarLink() {
+    return await this.aboutSidebarLink.isVisible();
+  }
+
+  async verifyLogoutSidebarLink() {
+    return await this.logoutSidebarLink.isVisible();
+  }
+
+  async verifyResetSidebarLink() {
+    return await this.resetSidebarLink.isVisible();
   }
 }
 
 
 
-            
+
 
