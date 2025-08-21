9b717384891c63cd1f822b02137a75934cf2ba8d
Shagun Singhal <shagun.singhal@gspann.com>
2025-08-21 12:25:58 +0530

    resolved some more conflicts

--- pages/loginPage.js
+++ pages/loginPage.js
@@ -5,21 +5,12 @@ export class loginPage{
     this.passwordLoc = page.locator('[data-test="password"]')
     this.loginBtnLoc = page.locator('[data-test="login-button"]')
     }
-<<<<<<< HEAD
  
-=======
-
->>>>>>> 6f25bbec33e1722b217617318e8e2444bb6ac2cf
     async login(username,password){
        await this.page.goto('https://www.saucedemo.com/')
        await this.userNameLoc.fill(username)
        await this.passwordLoc.fill(password)
        await this.loginBtnLoc.click()
-<<<<<<< HEAD
    
  
-=======
-    
-
->>>>>>> 6f25bbec33e1722b217617318e8e2444bb6ac2cf
 }}
--- tests/pageFactory.js
+++ tests/pageFactory.js
@@ -1,51 +1,15 @@
 import {test as base,expect} from '@playwright/test'
 import { cartPage } from '../pages/cartPage';
 import { checkoutOnePage } from '../pages/checkoutOnePage';
-<<<<<<< HEAD
 import {ProductPage} from '../pages/productPage'
 import { checkoutTwoPage } from '../pages/checkoutTwoPage';
 import { loginPage } from '../pages/loginPage';
-=======
-import { productPage } from '../pages/productPage';
-import { checkoutTwoPage } from '../pages/checkoutTwoPage';
-import { loginPage } from '../pages/loginPage';
-
->>>>>>> 6f25bbec33e1722b217617318e8e2444bb6ac2cf
 export const test=base.extend({
     loginpage:async ({page},use)=>{
         await use(new loginPage(page))
     },
     productpage:async ({page},use)=>{
-        await use(new ProductPage(page))import {test as base,expect} from '@playwright/test'
-        import { cartPage } from '../pages/cartPage';
-        import { checkoutOnePage } from '../pages/checkoutOnePage';
-        import {ProductPage} from '../pages/productPage'
-        import { checkoutTwoPage } from '../pages/checkoutTwoPage';
-        import { loginPage } from '../pages/loginPage';
-        export const test=base.extend({
-            loginpage:async ({page},use)=>{
-                await use(new loginPage(page))
-            },
-            productpage:async ({page},use)=>{
-                await use(new ProductPage(page))
-            },
-            cartpage:async ({page},use)=>{
-                await use(new cartPage(page))
-            },
-            checkoutonepage:async ({page},use)=>{
-                await use(new checkoutOnePage(page))
-            },
-            checkouttwopage:async ({page},use)=>{
-                await use(new checkoutTwoPage(page))  
-            },
-            pageSetUp:async ({loginpage},use)=>{
-                await loginpage.applaunching()
-                await loginpage.loginToAccount("visual_user","secret_sauce")
-                await loginpage.clickLogin()
-                await use();
-            }
-        })
-        export {expect}
+        await use(new ProductPage(page))
     },
     cartpage:async ({page},use)=>{
         await use(new cartPage(page))
@@ -57,8 +21,9 @@ export const test=base.extend({
         await use(new checkoutTwoPage(page))  
     },
     pageSetUp:async ({loginpage},use)=>{
-        
-        await loginpage.login("visual_user","secret_sauce")
+        await loginpage.applaunching()
+        await loginpage.loginToAccount("visual_user","secret_sauce")
+        await loginpage.clickLogin()
         await use();
     }
 })
