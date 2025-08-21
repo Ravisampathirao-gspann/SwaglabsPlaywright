14964adce4a4d1194ab2a807172fa77536355fdb
Pranshu Bhatnagar <pranshu.bhatnagar@gspann.com>
2025-08-21 11:17:42 +0530

    Completed all the test cases

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,17 +1,41 @@
 import { test, expect } from './pageFactory.js'
 
-test('login test', async ({loginpage,page})=>{
+test('Login with standard_user and valid password - @Pranshu', async ({loginpage,page})=>{
     await loginpage.login('standard_user','secret_sauce')
     await expect(page.locator('[data-test="title"]')).toBeVisible();
 })
 
-test('login test 2', async ({loginpage,page})=>{
+test('Login with problem_user and valid password - @Pranshu', async ({loginpage,page})=>{
     await loginpage.login('problem_user','secret_sauce')
     await expect(page.locator('[data-test="title"]')).toBeVisible();
 })
 
-test('login test 3', async ({loginpage,page})=>{
+test('Login with performance_glitch_user and valid password - @Pranshu', async ({loginpage,page})=>{
     await loginpage.login('performance_glitch_user','secret_sauce')
     await expect(page.locator('[data-test="title"]')).toBeVisible();
 })
 
+test('Login with error_user and valid password - @Pranshu', async ({loginpage,page})=>{
+    await loginpage.login('error_user','secret_sauce')
+    await expect(page.locator('[data-test="title"]')).toBeVisible();
+})
+
+test('Login with visual_user and valid password - @Pranshu', async ({loginpage,page})=>{
+    await loginpage.login('visual_user','secret_sauce')
+    await expect(page.locator('[data-test="title"]')).toBeVisible();
+})
+
+test('Login fails with invalid username - @Pranshu', async ({loginpage,page})=>{
+    await loginpage.login('wrong_user','secret_sauce')
+    await expect(page.locator('[data-test="error"]')).toBeVisible();
+})
+
+test('Login fails with invalid password - @Pranshu', async ({loginpage,page})=>{
+    await loginpage.login('standard_user','wrong_password')
+    await expect(page.locator('[data-test="error"]')).toBeVisible();
+})
+
+test('Login fails with locked out user - @Pranshu', async ({loginpage,page})=>{
+    await loginpage.login('locked_out_user','secret_sauce')
+    await expect(page.locator('[data-test="error"]')).toBeVisible();
+})
